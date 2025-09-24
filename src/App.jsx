f51f34cf39faeada8f5a21d98263b93e32601c9b
abitha000 <abithak000@yahoo.com>
2025-09-24 08:45:48 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import About from './components/About'
-import Services from './components/Services'
-import Booking from './components/Booking'
-import Contact from './components/Contact'
-import Footer from './components/Footer'
+import Navbar from './components/Navbar.jsx'
+import Hero from './components/Hero.jsx'
+import About from './components/About.jsx'
+import Services from './components/Services.jsx'
+import Booking from './components/Booking.jsx'
+import Contact from './components/Contact.jsx'
+import Footer from './components/Footer.jsx'
 
 export default function App() {
   return (
