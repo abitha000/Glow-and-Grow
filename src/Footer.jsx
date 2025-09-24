ae0b0b7806b9045ff704146555c37d50e336c50c
abitha000 <abithak000@yahoo.com>
2025-09-24 08:51:05 +0530

    Delete src directory

--- src/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Footer() {
-  return (
-    <footer className="bg-green-600 text-white text-center py-4 mt-10">
-      <p>© {new Date().getFullYear()} Glow and Grow. All rights reserved.</p>
-    </footer>
-  )
-}
